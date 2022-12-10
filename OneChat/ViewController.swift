0ef0a1574d50db3c61b1c77c3a323f68cfbaaaa7
donVolodya <volodymyr.novosad.knm.2020@lpnu.ua>
2022-12-10 13:14:43 +0200

    Initial Commit

---
+++ OneChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OneChat
+//
+//  Created by Vova Novosad on 10.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
