7b9f1292fed2dda1c5ffe5a3cb675e2ff4d752a1
donVolodya <volodymyr.novosad.knm.2020@lpnu.ua>
2022-12-12 21:25:27 +0200

    2nd user & design improvements

--- OneChat/Controllers/ChatVC.swift
+++ OneChat/Controllers/ChatVC.swift
@@ -4,10 +4,9 @@ import FirebaseAuth
 import FirebaseCore
 import FirebaseFirestore
 
-class ChatVC: UIViewController, UITableViewDataSource {
+class ChatVC: UIViewController {
     
-    @IBOutlet weak var chatTableView: UITableView!
-    
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageField: UITextField!
     
     let database = Firestore.firestore()
@@ -17,23 +16,114 @@ class ChatVC: UIViewController, UITableViewDataSource {
         super.viewDidLoad()
         
         navigationItem.hidesBackButton = true
-        chatTableView.dataSource = self
-        chatTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "messageCell")
-
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "messageCell")
+        
+        loadMessages()
     }
     
-//MARK: - TableView functions
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return messages.count
-       }
-       
-       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-           let cell = tableView.dequeueReusableCell(withIdentifier: "messageCell", for: indexPath) as! MessageCell
-           cell.messageLabel.text = messages[indexPath.row].text
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        do{
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true) }
+        catch {
+            print("Sign out error")
+        }
+    }
+    
+    
+    //MARK: - Working with data
+    
+    @IBAction func sendButtonPressed(_ sender: UIButton) {
+        
+        if let message = messageField.text, let userSender = Auth.auth().currentUser?.email
+        {
+            database.collection("messages").addDocument(data: ["sender" : userSender, "text" : message, "date" : Date().timeIntervalSince1970])
+            { error in
+                if let e = error
+                {
+                    print("Error, especially",e)
+                }
+                else
+                {
+                    print("Data saved succesfully")
+                }
+            }
+        }
+        messageField.text = ""
+    }
+    
+    
+    func loadMessages()
+    {
+        messages = []
+        database.collection("messages")
+            .order(by: "date")
+            .addSnapshotListener {querySnapshot, error in
            
-           return cell
-       }
-
-
+            self.messages = []
+            if let err = error {
+                    print("Error getting documents: \(err)")
+                } else {
+                        if let snaphotDocuments = querySnapshot?.documents
+                        {
+                            for doc in snaphotDocuments
+                            {
+                                let data = doc.data()
+                                if let sender = data["sender"] as? String
+                                    , let body = data["text"] as? String
+                                {
+                                    let newMessage = Message(sender: sender, text: body)
+                                    self.messages.append(newMessage)
+                                    
+                                    DispatchQueue.main.async {
+                                        self.tableView.reloadData()
+                                        let indexPath = IndexPath(row: self.messages.count - 1, section: 0)
+                                        self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        
 }
+
+
+
+//MARK: - TableView functions
+extension ChatVC : UITableViewDataSource
+{
+    
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return messages.count
+           }
+           
+           func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+               let cell = tableView.dequeueReusableCell(withIdentifier: "messageCell", for: indexPath) as! MessageCell
+               let message = messages[indexPath.row]
+               
+               cell.messageLabel.text = message.text
+               
+               if message.sender == Auth.auth().currentUser?.email
+               {
+                   cell.leftImage.isHidden = false
+                   cell.rightImage.isHidden = true
+                   cell.messageBubble.backgroundColor = .cyan
+                   cell.messageLabel.textColor = .black
+               }
+               else
+               {
+                   cell.rightImage.isHidden = false
+                   cell.leftImage.isHidden = true
+                   cell.messageBubble.backgroundColor = .purple
+                   cell.messageLabel.textColor = .white
+               }
+               
+               return cell
+           }
+}
+    
+
--- OneChat/View/MessageCell.swift
+++ OneChat/View/MessageCell.swift
@@ -1,9 +1,4 @@
-//
-//  MessageCell.swift
-//  OneChat
-//
-//  Created by Vova Novosad on 11.12.2022.
-//
+
 
 import UIKit
 
@@ -12,16 +7,15 @@ class MessageCell: UITableViewCell {
     @IBOutlet weak var messageBubble: UIView!
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var rightImage: UIImageView!
+    @IBOutlet weak var leftImage: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
 }
